8683fed6f35bf2e64be7e59ef289f121bd6829d7
krypton-0x00 <shakirgulzar0x00@gmail.com>
2024-11-12 22:14:10 +0530

    UPDATE: New Plugins

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -115,47 +115,47 @@ return {
 			jsonls = {},
 			yamlls = {},
 			-- TypeScript/JavaScript
-			-- tsserver = {
-			-- 	settings = {
-			-- 		typescript = {
-			-- 			inlayHints = {
-			-- 				includeInlayParameterNameHints = "all",
-			-- 				includeInlayParameterNameHintsWhenArgumentMatchesName = false,
-			-- 				includeInlayFunctionParameterTypeHints = true,
-			-- 				includeInlayVariableTypeHints = true,
-			-- 				includeInlayPropertyDeclarationTypeHints = true,
-			-- 				includeInlayFunctionLikeReturnTypeHints = true,
-			-- 				includeInlayEnumMemberValueHints = true,
-			-- 			},
-			-- 		},
-			-- 		javascript = {
-			-- 			inlayHints = {
-			-- 				includeInlayParameterNameHints = "all",
-			-- 				includeInlayParameterNameHintsWhenArgumentMatchesName = false,
-			-- 				includeInlayFunctionParameterTypeHints = true,
-			-- 				includeInlayVariableTypeHints = true,
-			-- 				includeInlayPropertyDeclarationTypeHints = true,
-			-- 				includeInlayFunctionLikeReturnTypeHints = true,
-			-- 				includeInlayEnumMemberValueHints = true,
-			-- 			},
-			-- 		},
-			-- 	},
-			-- },
-			--
-			-- -- ESLint
-			-- eslint = {
-			-- 	settings = {
-			-- 		-- Specify package manager if needed
-			-- 		packageManager = "npm",
-			-- 	},
-			-- 	on_attach = function(client, bufnr)
-			-- 		-- Enable formatting
-			-- 		vim.api.nvim_create_autocmd("BufWritePre", {
-			-- 			buffer = bufnr,
-			-- 			command = "EslintFixAll",
-			-- 		})
-			-- 	end,
-			-- },
+			ts_ls = {
+				settings = {
+					typescript = {
+						inlayHints = {
+							includeInlayParameterNameHints = "all",
+							includeInlayParameterNameHintsWhenArgumentMatchesName = false,
+							includeInlayFunctionParameterTypeHints = true,
+							includeInlayVariableTypeHints = true,
+							includeInlayPropertyDeclarationTypeHints = true,
+							includeInlayFunctionLikeReturnTypeHints = true,
+							includeInlayEnumMemberValueHints = true,
+						},
+					},
+					javascript = {
+						inlayHints = {
+							includeInlayParameterNameHints = "all",
+							includeInlayParameterNameHintsWhenArgumentMatchesName = false,
+							includeInlayFunctionParameterTypeHints = true,
+							includeInlayVariableTypeHints = true,
+							includeInlayPropertyDeclarationTypeHints = true,
+							includeInlayFunctionLikeReturnTypeHints = true,
+							includeInlayEnumMemberValueHints = true,
+						},
+					},
+				},
+			},
+
+			-- ESLint
+			eslint = {
+				settings = {
+					-- Specify package manager if needed
+					packageManager = "npm",
+				},
+				on_attach = function(client, bufnr)
+					-- Enable formatting
+					vim.api.nvim_create_autocmd("BufWritePre", {
+						buffer = bufnr,
+						command = "EslintFixAll",
+					})
+				end,
+			},
 
 			-- Lua
 			lua_ls = {
